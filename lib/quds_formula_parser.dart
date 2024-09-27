72b289ad66e1210fba1148cd34640b178cf2db7a
MohammedAsaadAsaad <m.a.asaad@hotmail.com>
2024-09-27 08:49:50 +0300

    Modifying the main documentation of library;

--- lib/quds_formula_parser.dart
+++ lib/quds_formula_parser.dart
@@ -1,6 +1,18 @@
-/// Support for doing something awesome.
+/// Quds Formula Parser is a powerful Dart package designed to parse, evaluate, and manipulate mathematical formulas in various forms. It supports diverse data types, including numbers, dates, and custom objects, offering a flexible solution for formula-based calculations.
 ///
-/// More dartdocs go here.
+/// For example:
+/// ```dart
+/// import  'package:quds_formula_parser/quds_formula_parser.dart';
+///
+/// void  main() {
+/// FormulaParser  parser  =  FormulaParser(provider:  FormulaProvider.defaultInstance);
+/// var  formula  =  parser.parse('(0.5)^Sin(pi/2)');
+/// var  supporter  =  FormulaInfixToPostfixConvertor(formula:  formula);
+/// var  result  =  supporter.evaluate();
+/// print(result.value);
+/// }
+/// ```
+
 library;
 
 //Internal
